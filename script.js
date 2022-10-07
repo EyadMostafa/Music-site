992f4250487f0acd9d86500abe919f31269233cb
EyadMostafa <79060024+EyadMostafa@users.noreply.github.com>
2022-10-07 15:35:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// $(window).on("unload", function () {
+//   $(window).scrollTop(0);
+// });
+
+// window.addEventListener("scroll", () => {
+//   const scrolled = window.scrollY;
+//   console.log(scrolled);
+// });
